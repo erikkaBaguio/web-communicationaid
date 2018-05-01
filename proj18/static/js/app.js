4389d2fbf6bb1dbad8dd87caa643cb4734d8a963
unknown <yumielouise@gmail.com>
2018-05-01 15:40:26 +0800

    created ajax for posting progress report

--- proj18/static/js/app.js
+++ proj18/static/js/app.js
@@ -1,73 +1,25 @@
-
-function edit_parent() {
-
-	$.ajax(
-		{
-			url: 'http://127.0.0.1:5000/edit_parent/<int:acc_id>',
-			contentType: 'application/json; charset=utf-8',
-			data: JSON.stringify({
-				'fname_p': $("#fname_p").val(),
-				'lname_p': $("#lname_p").val(),
-				'bday_p': $("#bday_p").val(),
-				'add_p': $("#add_p").val()
-			}),
-			type: "POST",
-			dataType: "json",
-			error: function (e) {
-			},
-			success: function (resp) {
-                if (resp.status == 'ok') {
-                	alert("Successfully updated!")
-                    window.location.replace('childinfo.html')
-
-                 }
-				else {
-					alert("ERROR")
-				}
-
-			}
-		});
-}
-
-function getinfo_parent(acc_id){
-
-	 $("#parent").show();
-	 alert('hey!!')
-
+function progress() {
     $.ajax({
-    		url: 'http://127.0.0.1:5000/parent/'+acc_id,
-    		type: "GET",
-    		dataType: "json",
-    		success: function(resp) {
+        url: 'http://127.0.0.1:5000/api/educational/progress/',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({
+            'prog_num': $("#prog_num").val(),
+            'title': $("#title").val(),
+            'details': $("#details").val(),
+            'prog_date': $("#prog_date").val(),
+            'prog_time': $("#prog_time").val(),
+            'score': $("#score").val(),
+            'ed_num': $("#ed_num").val()
+        }),
+        type: "POST",
+        dataType: "json",
+        crossDomain: true,
+        success: function (resp) {
+                console.log("success")
 
-				if (resp.status  == 'ok') {
-				   for (i = 0; i < resp.count; i++)
-                                  {
-									   fname_p = resp.entries[i].fname_p;
-									   lname_p = resp.entries[i].lname_p;
-									   bday_p = resp.entries[i].bday_p;
-									   add_p = resp.entries[i].add_p;
-                                       $("#parent").append(parent(fname_p,lname_p, bday_p, add_p));
-
-                                  }
-
-
-
-				} else
-				{
-                                       $("#parent").html("");
-					alert(resp.message);
-				}
-    		}
-		});
-}
-
-function parent(fname_p,lname_p,bday_p,add_p)
-{
-   return '<div class="col-md-12 col-sm-12 col-xs-12">' +
-       '<p>First Name: '+ ' ' +fname_p+  '</p>'+
-       '<p>Last Name:'+ ' ' +lname_p+ '</p>'+
-       '<p>Birthdate:'+ ' ' +bday_p+ '</p>'+
-       '<p>Age:'+ ' ' +add_p+ '</p>'+
-       '</div>'
+        },
+        error: function (resp) {
+            window.location.replace('/texts/404.html');
+        }
+    });
 }
