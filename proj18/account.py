a4c1c2c0c8d1da53ab5e91f07c46c986de494300
unknown <yumielouise@gmail.com>
2018-05-23 22:04:21 +0800

    numbers audio is now playable

--- proj18/account.py
+++ proj18/account.py
@@ -17,7 +17,7 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route('/')
 def welcome():
     # session['user'] = 'me'
-    return render_template('alphabet.html')
+    return render_template('numbers.html')
 
 @app.route('/register')
 def register():
