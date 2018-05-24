a64f941272f086c1c00867048bacad962bb7bedc
unknown <yumielouise@gmail.com>
2018-05-24 11:33:42 +0800

    modified dashboard

--- proj18/account.py
+++ proj18/account.py
@@ -17,7 +17,7 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route('/')
 def welcome():
     # session['user'] = 'me'
-    return render_template('login.html')
+    return render_template('p_mode.html')
 
 @app.route('/register')
 def register():
