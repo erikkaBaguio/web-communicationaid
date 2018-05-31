766f288ad3aa98d6e8b2c2d83611eb4221343e28
unknown <yumielouise@gmail.com>
2018-05-31 16:58:54 +0800

    added activity log ui

--- proj18/account.py
+++ proj18/account.py
@@ -17,7 +17,7 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route('/')
 def welcome():
     # session['user'] = 'me'
-    return render_template('educational.html')
+    return render_template('activityLog.html')
 
 @app.route('/register')
 def register():
